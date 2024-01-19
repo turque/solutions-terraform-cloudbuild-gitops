be384d8752f6769dd916a8f8ba180c40b860fa2d
Arnaldo Turque <arnaldo@turque.com.br>
2024-01-18 21:03:04 -0300

    Fix vpc

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -46,35 +46,35 @@ module "vpc" {
         }
     ]
 
-    secondary_ranges = {
-        subnet-01 = [
-            {
-                range_name    = "${var.env}-subnet-01-secondary-01"
-                ip_cidr_range = "192.168.64.0/24"
-            },
-        ]
+    # secondary_ranges = {
+    #     subnet-01 = [
+    #         {
+    #             range_name    = "${var.env}-subnet-01-secondary-01"
+    #             ip_cidr_range = "192.168.64.0/24"
+    #         },
+    #     ]
 
-        subnet-02 = []
-    }
+    #     subnet-02 = []
+    # }
 
-    routes = [
-        {
-            name                   = "egress-internet"
-            description            = "route through IGW to access internet"
-            destination_range      = "0.0.0.0/0"
-            tags                   = "egress-inet"
-            next_hop_internet      = "true"
-        },
-        {
-            name                   = "app-proxy"
-            description            = "route through proxy to reach app"
-            destination_range      = "10.50.10.0/24"
-            tags                   = "app-proxy"
-            next_hop_instance      = "app-proxy-instance"
-            next_hop_instance_zone = "us-west1-a"
-        },
-    ]
-}
+#     routes = [
+#         {
+#             name                   = "egress-internet"
+#             description            = "route through IGW to access internet"
+#             destination_range      = "0.0.0.0/0"
+#             tags                   = "egress-inet"
+#             next_hop_internet      = "true"
+#         },
+#         {
+#             name                   = "app-proxy"
+#             description            = "route through proxy to reach app"
+#             destination_range      = "10.50.10.0/24"
+#             tags                   = "app-proxy"
+#             next_hop_instance      = "app-proxy-instance"
+#             next_hop_instance_zone = "us-west1-a"
+#         },
+#     ]
+# }
 
 # module "vpc" {
 #   source  = "terraform-google-modules/network/google"
